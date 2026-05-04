2febe3a72b239b3c012f5f3cdc9ae079211eb7e7
José Alarcón <jose.alarconan@supereduc.cl>
2026-05-04 16:07:22 -0400

    Assets + Footer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,7 @@
 
 ## [1.0.0] - 29/04/2026
 - Proyecto base
+## [1.0.0] - 04/05/2026
+- Arquitectura base
+- Añade Footer
+- Añade Assets
--- src/app/shared/components/layout/auth-layout/auth-layout.ts
+++ src/app/shared/components/layout/auth-layout/auth-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet], // 👈 CLAVE
+  imports: [RouterOutlet,FooterComponent],
   templateUrl: './auth-layout.html',
   styleUrls: ['./auth-layout.css']
 })
