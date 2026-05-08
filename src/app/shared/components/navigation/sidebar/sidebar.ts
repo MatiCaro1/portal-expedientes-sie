0bf769a2d9d3d78e1776e056450dcba97d348475
José Alarcón <jose.alarconan@supereduc.cl>
2026-05-08 10:24:18 -0400

    detalles

--- src/app/shared/components/navigation/sidebar/sidebar.ts
+++ src/app/shared/components/navigation/sidebar/sidebar.ts
@@ -23,7 +23,7 @@ export class SidebarComponent {
   };
 
   navItems: NavItem[] = [
-    { label: 'Home', route: '/app/inicio', icon: 'home' }, //Preseleccionado
+    { label: 'Inicio', route: '/app/inicio', icon: 'home' }, //Preseleccionado
     { label: 'Mis Expedientes', route: '/expedientes', icon: 'inbox', badge: '3', subtitle: '3 expedientes activos' },
     { label: 'Configuración', route: '/configuracion', icon: 'settings' },
     //{ label: 'Ayuda', route: '/ayuda', icon: 'help' },
