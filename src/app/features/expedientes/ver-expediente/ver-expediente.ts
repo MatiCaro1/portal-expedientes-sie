b7b5f76ef11bf0696819e5a85281c41104cf0c99
José Alarcón <jose.alarconan@supereduc.cl>
2026-05-11 14:44:44 -0400

    feat-verExpediente

--- src/app/features/expedientes/ver-expediente/ver-expediente.ts
+++ src/app/features/expedientes/ver-expediente/ver-expediente.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ExpedienteTabs } from '../expediente-tabs/expediente-tabs';
 
 @Component({
   selector: 'app-ver-expediente',
-  imports: [],
+  imports: [RouterOutlet,ExpedienteTabs],
   templateUrl: './ver-expediente.html',
   styleUrl: './ver-expediente.css',
 })
